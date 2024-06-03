1660fb8907ab8dab43d8bf87e12c6ee2bbf2079f
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-03 10:21:17 +0500

    first commit

---
+++ EX-JS/claass 9/app.js
@@ -0,0 +1,26 @@
+//  NESTED LOOPP
+ 
+//  let arr = [" Ali", "Ba","ALI", "SUN"]
+// let  arr2 = ["Expritzo", "Ali"]
+// for (let i = 0; i <arr.length ; i++) {
+//     for (let j= 0; j < arr2.length ; j++) {
+//         console.log (arr[i] + "" + arr [j] + "<br>")
+//     }
+// }
+
+
+// for (let i =1; i <=100 ; i= i+10) {
+//     for (let j = i ; j <= i ; j++) {
+//         document.write (j + "")
+//     }
+//     document.write(j )
+
+// }
+
+// for (let i = 1; i <=5; i++) {
+//     for (let j= 1; j  <= i ; j++ ) {
+//         document.write (j + "")
+//     }
+//     document.write ("<br> ")
+// } 
+
