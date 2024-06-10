5ee8f15ea0d8653d21557f544bc8ac69aa16c31c
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-11 02:15:11 +0500

    continue

---
+++ EX-JS/claass 13/app.js
@@ -0,0 +1,14 @@
+// function onbulb (){
+//     document.getElementById(on).src = 'https://www.shutterstock.com/image-vector/glowing-yellow-light-bulb-inspiration-260nw-83128966.jpg'
+  
+// }
+
+// function offbulb () {
+//     document.getElementById(of).src = 'https://w7.pngwing.com/pngs/859/425/png-transparent-light-bulb-illustration-incandescent-light-bulb-lamp-bulb-candle-product-light-thumbnail.png'
+// }
+
+// function sum () {
+//     let getInp = document.getElementById("inp").value
+//     console.log (getInp)
+// }
+
