2542fc9eb65f9a49b0fc7910ded8d9cc6f860330
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-20 12:40:53 +0500

    first comit

---
+++ EX-JS/class 10/app.js
@@ -0,0 +1,96 @@
+// let game = ["Sheikh", "Abdullah", "Abdul", "Sameed"];
+// let game2 = prompt ("Enter Your Name");
+// let Hum = "";
+// let upperCase =game2.slice (0.1)
+// let lowerCase = game2.slice (1) 
+// let resuil = upperCase+ lowerCase;
+
+// for (let i = 0; i < game.length ; i++) {
+//     if  (final == game[i]) {
+//         Hum = "yes"
+//         alert (final + "Name Not Foud")
+//     }
+    
+// }
+
+// if (match == "") {
+//     alert(final + "This is not found")
+// }
+
+
+// let game = ["Sheikh", "Abdullah", "Abdul", "Sameed"];
+// let game2 = prompt("Enter Your Name");
+// let Hum = "";
+// let upperCase = game2.slice(0, 1).toUpperCase();
+// let lowerCase = game2.slice(1).toLowerCase();
+// let final = upperCase + lowerCase;
+
+// for (let i = 0; i < game.length; i++) {
+//     if (final === game[i]) {
+//         Hum = "yes";
+//         alert(final + " Name Found");
+//         break; 
+//     }
+// }
+
+// if (Hum === "") {
+//     alert(final + " This name is not found");
+// }
+
+
+// let game = ["Gamer", "Sheikh", "Abdullah", "Abdul", "Ahemd"];
+// let game2 = prompt ("Enter Your Nmae"); 
+// let hum = ""; 
+// let upperCase = game2.slice (0 , 1).toUpperCase();
+// let lowerCase = game2.slice (1).toLowerCase ();
+// let final = upperCase+lowerCase;
+
+
+// for (let i =0; i < game.length ; i++) {
+//     if (final === game [i]) {
+//         hum = "yes"
+//         alert (final + "Name To be Found")
+//     }
+    
+// }
+// if (hum === "") {
+//     alert (final + "This Is Not Found")
+// }
+
+
+// let game = ["Abdullah", "Sheikh", 'Abdul', "Sheikh Abdullah", "House"]; 
+// let game2 = prompt ('Enter Your Number');
+// hum =""; 
+// let upperCase = game2.slice (0, 1).toUpperCase();
+// let lowerCase = game2.slice (1).toLowerCase();
+// let final = upperCase+ lowerCase;
+
+// for (let i = 0; i <= game.length ; i++) {
+//     if (final === game [i]  ) {
+//         hum = "yes"
+//         alert ("This User IS Found")
+//     }
+// }
+
+// if (final  === "" ) {
+//     alert ("This User IS Not FOund")
+// }
+
+
+
+
+
+
+
+
+
+
+// let game = prompt ("Enter The Player Name");
+// let game2 = prompt ("Enter The Player 2");
+// let toss = Math.random () * 2
+// let convert = Math.floor (toss)
+
+// if (convert == 0) {
+//     alert (game + "head Win")
+//     alert (game2 + "tails Win")
+// }
