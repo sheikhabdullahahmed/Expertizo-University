daef0501746ee5932d3dbc517eda57729ae2039f
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-22 12:45:10 +0500

    continue

---
+++ EX-JS/class 11/app.js
@@ -0,0 +1,141 @@
+// function printName() {
+//     document.write("ddd")
+// }
+
+// printName () 
+// printName ()
+
+// function print () {
+//     for (let i = 1; i<=20; i++ ) {
+//         alert("hello world")
+//     }
+    
+// }
+// print()
+
+// function print () {
+//     for (let i = 1; i <= 20; i++) {
+//         document.write("gamer")
+//         document.write ("<br>")
+//     }
+// }
+
+// print ()
+// function print () {
+//     for (let i =0; i <= 9; i++) {
+//         document.write("Gamer")
+//         document.write ("<br>")
+//     }
+// }
+
+// print ()
+
+
+
+
+
+
+// function print () {
+//     let arry = ["Sheikh", "Abdullah", "Sheikh Abdullah"]
+//     let arry2 = ["Gamer", "Home"]
+
+//     for (let i = 0; i <= arry.length; i++) {
+//         for (let j = i ; j <= arry2.length; j++) {
+//             document.write (arry [i] + "" + arry2[j])
+//             document.write("<br>")
+//         }
+//     }
+// }
+// print()
+
+
+// function sum (a , b ) {
+//     alert (a + b)
+// }
+
+// sum (10 ,  20) 
+
+// function sum (a, b, c= 5, d= 10) {
+//     alert(a - b - c - d)
+// }
+
+// sum (prompt ("Enter value 1" , prompt ("ENter value 1"), prompt("enter your value 1"), prompt("ee")) )
+
+// function sum (a , b) {
+//     alert (a + b)
+
+// }
+// sum (10 , 20)
+// function sum (a , b, c=5, d= 7) {
+//     alert (a - b - c -d) 
+// }
+// sum (prompt ("Enter Value 1" , prompt ("Enter Value of 1"), prompt ("Enter Your Value of 1"))) 
+ 
+
+
+// function abc () {
+//     return "bb"
+// }
+// console.log(abc())
+
+// function abc () {
+//     return "Abdullah"
+// }
+// console.log(abc ())
+
+// function abc () {
+//     return 'Abdullah Sheikh'
+// }
+
+// console.log (abc ())
+
+
+
+
+
+// function abc (a , b) {
+//     return "bb"
+// }
+// console.log(abc())
+
+
+// function game   (a , b) {
+//     prompt ("Enter Your 1st Value") 
+//     prompt ("Enter Your 2nd Value")
+//     // prompt + ("a + b "=" game ") 
+//     return ( "Enter Your 1st Value" + "Enter Your 1st Value" + + + "=" + game)
+// }
+// game (10, 20)
+
+// document.write (game ())
+
+
+// function abc () {
+//     var a = "helll" 
+//     console.log (a)
+// }
+// abc ()
+
+// function abc () {
+//     let a = "hellow World"
+//     let b = "Cat"
+//     let com = a + b
+//     console.log (com)
+// }
+
+// abc ()
+
+
+// var printDate = new Date ()
+// var convert = printDate.toString().slice (0 , 15)
+// document.write(convert)
+
+// var printDate = new Date ()
+// var convert = printDate.toString().slice(16, 24)
+// document.write(convert)
+
+
+
+
+
+
