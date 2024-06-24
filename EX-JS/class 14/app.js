0b95c6170294dde83bab4bf4dcab3ee9fcf97314
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-25 01:33:36 +0500

    continue

---
+++ EX-JS/class 14/app.js
@@ -0,0 +1,459 @@
+// let milisecc = 0;
+// let sec  = 0; 
+// let min = 0;
+// let getmilisec= document.getElementById ("msec")
+// let getmin = document.getElementById ("min")
+// let getsec = document.getElementById ("sec")
+// let interval;
+
+
+// function start () {
+//     interval = setInterval(function () {
+//         milisecc++
+//         getmsec.innerHTML = milisecc
+//         if (milisecc >= 100) {
+//             sec++
+//             getsec.innerHTML= sec
+//             milisecc = 0
+//         } 
+//         else if (sec >=10) {
+//             min++ 
+//             getmin.innerHTML = min
+//             sec = 0 
+//         }
+//         document.getElementById("btn").disabled = true
+
+//      }, 10)
+// }
+
+// function stop (){
+//     clearInterval(interval)
+//     document.getElementById("btn").disabled = false
+// } 
+// function reset () {
+//     milisecc = 0;
+//     min = 0;
+//     sec = 0;
+//     getmin.innerHTML = min
+//     getmsec.innerHTML = msec
+//     getsec.innerHTML = sec
+// }
+
+
+// var minutes = 0
+// var seconds = 0
+// var milisec = 0
+// var getmin = document.getElementById("min")
+// var getsec = document.getElementById("sec") 
+// var getmsec = document.getElementById("msec") 
+// var interval;
+
+//  function start(){
+//     interval = setInterval(function(){
+//         milisec++
+//         getmsec.innerHTML = milisec
+//         if(milisec >= 100){
+//             seconds++
+//             getsec.innerHTML = seconds
+//             milisec = 0
+//         }
+//         else if(seconds >= 10){
+//             minutes++
+//             getmin.innerHTML = minutes
+//             seconds = 0
+//         }
+//         document.getElementById("btn").disabled = true
+//     },10)
+// }
+// function stop(){
+//     clearInterval(interval)
+//     document.getElementById("btn").disabled = false
+// }
+
+// function reset(){
+//     milisec = 0
+//     minutes = 0
+//     seconds = 0
+//     getmin.innerHTML = minutes
+//     getmsec.innerHTML = milisec
+//     getsec.innerHTML = seconds
+// }
+
+
+// let minutes = 0;
+// let second = 0;
+// let msecond = 0;
+// let getmin = document.getElementById("min")
+// let getsec = document.getElementById("sec")
+// let getmsec = document.getElementById("msec")
+// let interval ;
+
+// function start () {
+//     interval = setInterval (function () {
+//         msecond++
+//         getsec.innerHTML = msecond
+//         if (msecond >= 100) {
+//             second++
+//             getsec.innerHTML = second
+//             msecond = 0 
+//         }
+//         else if (second >= 10) {
+//             minutes++
+//             getmin.innerHTML = minutes
+//             second = 0 
+//         }
+//     }, 10)
+// } 
+// function stop () {
+//     clearInterval(interval)
+//     document.getElementById("btn").disabled = false
+// }
+// function reset () {
+//     msecond = 0;
+//     minutes = 0;
+//     second = 0;
+//     getmin.innerHTML = minutes
+//     getmsec.innerHTML=msecond
+//     getsec.innerHTML = second
+// }
+ 
+
+// let minutes = 0;
+// let second = 0;
+// let milisec = 0; 
+// let getmin = document.getElementById("min")
+// let getsec = document.getElementById("sec")
+// let getmsec = document.getElementById("msec")
+// let interval;
+
+// function start () {
+//     interval = setInterval (function () {
+//         milisec++
+//         getmin.innerHTML = milisec
+//         if(milisec >= 100) {
+//             second++ 
+//             getsec.innerHTML =second
+//             milisec = 0       
+//          }
+//          else if(second >= 10) {
+//             minutes++
+//             getmin.innerHTML = minutes
+//             second = 0
+//          }
+//          document.getElementById ("btn").disabled = true 
+//     }, 10)
+// }
+// function stop() {
+//     clearInterval (interval)
+//     document.getElementById("btn").disabled = false
+// }
+
+
+// function reset () {
+//     milisec = 0;
+//     minutes = 0;
+//     second  = 0; 
+//     getmin.innerHTML= minutes;
+//     getmsec.innerHTML = milisec;
+//     getsec.innerHTML = second ; 
+
+// }
+
+// let minutes = 0; 
+// let second = 0 ;
+// let milisecc = 0;
+// let getmin= document.getElementById("min");
+// let getsec = document.getElementById("sec")
+// let getmsec = document.getElementById("msec")
+// let interval ;
+  
+// function start() {
+//     interval = setInterval (function () {
+//         milisecc ++
+//         getmin.innerHTML = milisecc
+//         if (milisecc >= 100) {
+//             second ++
+//             getsec.innerHTML = second
+//             milisecc = 0 
+//         }
+//         else if (second >= 10) {
+//             minutes++ 
+//             getmin.innerHTML = 0
+//             second = 0
+//         }
+//         document.getElementById ("btn").disabled = true
+        
+
+        
+//     }, 10)
+// }
+// function stop () {
+//     clearInterval (interval) 
+//     document.getElementById ("btn").disabled = false
+// }
+
+// function reset () {
+//     milisecc = 0;
+//     minutes = 0;
+//     second = 0;
+//     getmin.innerHTML = minutes;
+//     getsec.inert = second ;
+//     getmsec.inert = milisecc;
+// }
+
+// let minutes = 0;
+// let seconds = 0;
+// let milisecc = 0;
+// let getmin = document.getElementById("min")
+// let getsec = document.getElementById ("sec")
+// let getmilisec = document.getElementById ("msec")
+// let interval;
+
+// function start () {
+//     interval = setInterval (function () {
+//         milisecc ++
+//         getmin.innerHTML = milisecc
+//         if (milisecc >=100) {
+//             seconds ++
+//             getsec.innerHTML = seconds
+//             milisecc = 0
+//         }
+//         else if (seconds >=10) {
+//             minutes ++
+//             getmin.innerHTML = 0
+//             seconds = 0
+//         }
+
+//         document.getElementById ("btn").disabled = true
+
+//     }, 10)
+
+// }
+// function stop () {
+//     clearInterval (interval) 
+//     document.getElementById ("btn").disabled = false
+// }
+
+// function resest () {
+//     milisecc = 0
+//     minutes = 0
+//     seconds = 0
+//     getmin.innerHTML = minutes
+//     getmilisec.innerHTML = milisecc
+//     getsec.innerHTML = seconds
+
+// } 
+
+
+
+// let minutes = 0
+// let second = 0
+// let milisec = 0
+// let getmin = document.getElementById ("min")
+// let getsec = document.getElementById ("sec")
+// let getmsec = document.getElementById ("msec")
+// let interval ;
+
+// function start () {
+//     interval = setInterval (function (){
+//         milisec ++
+//         getsec.innerHTML = milisec
+//         if (milisec >=100) {
+//             second++ 
+//             getsec.innerHTML = second
+//             milisec = 0
+//         }
+//         else if (second >=10) {
+//             minutes++
+//             getmin.innerHTML = minutes
+//             second = 0
+//         }
+//         document.getElementById ("btn").disabled = true
+        
+//     }, 10)
+// } 
+
+// function stop () {
+//     clearInterval (interval) 
+//     document.getElementById ("btn").disabled = false
+// }
+// function reset () {
+//     minutes = 0
+//     milisec = 0
+//     second = 0
+//     getmin.innerHTML = minutes
+//     getsec.innerHTML = second
+//     getmsec.innerHTML = milisec
+// }
+
+
+// let minutes = 0;
+// let second = 0;
+// let milisec = 0;
+// let getmin = document.getElementById("min")
+// let getsec = document.getElementById("sec")
+// let getmsec = document.getElementById("msec")
+// let interval ;
+
+// function start () {
+//     interval = setInterval (function () {
+//         milisec++
+//         getmsec.innerHTML = milisec
+//         if (milisec >=100) {
+//             second++
+//             getsec.innerHTML = second
+//             milisec = 0
+
+//         }
+//         else if  (second >=10) {
+//             minutes++
+//             getmin.innerHTML = minutes
+//             second = 0
+//         }
+//         document.getElementById ("btn").disabled = true;
+//     })
+
+// }
+
+// function stop () {
+//     clearInterval = interval 
+//     document.getElementById ("btn").disabled = false;
+// }
+
+// function reset () {
+//     minutes = 0
+//     second = 0
+//     milisec = 0
+//     getmin.innerHTML = minutes 
+//     getsec.innerHTML = second 
+//     getmsec.innerHTML = milisec
+// }
+
+
+// let minutes = 0;
+// let second = 0; 
+// let milisec = 0;
+// let getmin = document.getElementById ("min")
+// let getsec = document.getElementById ("sec")
+// let getmsec = document.getElementById ("msec")
+// let interval ;
+
+// function start  () {
+//     interval = setInterval (function () {
+//         milisec++
+//         getmsec.innerHTML = milisec
+//         if (milisec >=100) {
+//             second++
+//             getsec.innerHTML = second
+//             milisec = 0
+//         }
+//         else if (second >= 10) {
+//             minutes++
+//             getmin.innerHTML = minutes
+//             second = 0
+//         }
+//     })
+// }
+
+
+// function start () {
+//     interval = setInterval (function () {
+//         milisec++
+//         getmsec.innerHTML =milisec
+//         if (milisec >=100) {
+//             second++
+//             getsec.innerHTML = second
+//             milisec = 0
+
+             
+//         }
+//         else if (second >=10) {
+//             minutes++
+//             getmin.innerHTML = minutes 
+//             second = 0
+//         }
+//         document.getElementById ("btn").disabled = true
+//     })
+// }
+
+
+// function stop () {
+//     clearInterval = interval
+//     document.getElementById ("btn").disabled = false
+// }
+
+
+// function reset () {
+//     minutes = 0;
+//     second = 0;
+//     milisec= 0;
+//     getmin.innerHTML = minutes 
+//     getsec.innerHTML = second
+//     getmilisec.innerHTML =  
+
+// }
+// let minutes = 0;
+// let second = 0;
+// let milisec = 0;
+// let getmin = document.getElementById ("min");
+// let getsec = document.getElementById ("sec");
+// let getmsec = document.getElementById ("msec");
+// let interval ;
+ 
+// function start () {
+//     interval = setInterval (function () {
+//         milisec++
+//         getmsec.innerHTML = milisec
+//         if (milisec >=100) {
+//             second++
+//             getsec.innerHTML = second
+//             milisec = 0
+//         } 
+//         else if(second >=10) {
+//             minutes++
+//             getmin.innerHTML = minutes
+//             second = 0
+//         }
+//         document.getElementById ("btn").disabled = true
+//     }, 10)
+// } 
+
+// function stop () {
+//     clearInterval = interval 
+//     document.getElementById ("btn").disabled = false
+// }
+// function reset () {
+//     milisec = 0
+//     minutes = 0
+//     second = 0
+//     getmsec.innerHTML = milisec;
+//     getmin.innerHTML  =minutes;
+//     getsec.innerHTML = second 
+// }
+
+// let minutes = 0 ;
+// let second = 0;
+// let milisec = 0;
+// let getmin =document.getElementById ("min")
+// let getsec =document.getElementById ("sec")
+// let getmsec =document.getElementById ("msec")
+// let interval ;
+  
+// function start () {
+//     interval = setInterval (function () {
+//         milisec+=
+//         getmsec.innerHTML = milisec;
+//         if (milisec >=100) {
+//             second++
+//             getsec.innerHTML = second
+//             milisec = 0
+//         }
+//         else if (second >=10) {
+//             minutes++
+//             getmin.innerHTML =minutes
+//             second =0
+//         }
+//         document.getElementById ("btn").disabled=true
+//     } , 10)
+// }
+
