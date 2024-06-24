6f0f13cd42b7536b24c9979d3acd2a712b1d14d6
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-24 10:34:34 +0500

    continue

---
+++ EX-JS/class 12/app.js
@@ -0,0 +1,58 @@
+// function age () {
+// let puraniTariq = prompt ("Enter Your Age") new Date().getTime;
+// let ajkiTariq = new Date ().getTime ();
+// let minus = ajkiTariq - puraniTariq ;
+// let formula = minus / (1000 * 60 * 60 * 24 * 365)
+// }
+
+// console.log (age ())
+
+// function age () {
+//     let puraniTariq = new Date (prompt ("Enter Your Birthday(5-dec-2004) ")).getTime()
+//     let ajkiTariq = new Date().getTime();
+//     let minus = ajkiTariq - puraniTariq;
+//     let formula = minus / (1000 * 60 * 60 * 24 * 365);
+//     return formula
+// }
+
+// console.log (age ());
+
+
+// let number = -23.44444;
+// console.log (Math.floor(number))
+
+
+// function absolutevalue (){
+//     if (num < 0) {
+//         return -num;
+//     }
+//     else {
+//         return num; 
+//     }
+// }
+
+// console.log (`absolutvalue is -4 is  ${absolutevalue}is -4`)
+// console.log (`absolutvalue is -4 is  ${absolutevalue}is 5 `)
+
+// function absoluteValue(num) {
+//     if (num < 0) {
+//         return -num;
+//     } else {
+//         return num;
+//     }
+// }
+
+// // Test the function
+// console.log("Absolute value of -4 is", absoluteValue(-4));
+// console.log("Absolute value of 5 is", absoluteValue(5));
+
+
+// let time = "Sheikh";
+// if (time == "pm") {
+//     console.log ("its pm")
+// }
+// else   {
+//     time == "am"
+//     alert("its am")
+// }
+        
