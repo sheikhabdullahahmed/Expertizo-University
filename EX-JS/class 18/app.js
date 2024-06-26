1f92492c9bc8fcdb6a04dd8b6c7c2a782b560a65
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-26 13:55:46 +0500

    continue

---
+++ EX-JS/class 18/app.js
@@ -0,0 +1,36 @@
+// let arr = [
+//     {
+//         p_id: 1,
+//         p_name : Audi,
+//         p_price : 2000,
+//         p_img : 'https://uploads.audi-mediacenter.com/system/production/media/119503/images/aeedd2295a3d388f0b68171a5862ce8d7e002fb5/A234735_web_2880.jpg?1698545884' 
+
+//     },
+//     {
+//         p_id: 2,
+//         p_name : mercedes,
+//         p_price : 3000,
+//         p_img : 'https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?cs=srgb&dl=pexels-mike-bird-2365572.jpg&fm=jpg' 
+
+//     },
+//     {
+//         p_id: 3,
+//         p_name : lambo,
+//         p_price : 4000,
+//         p_img : 'https://media.istockphoto.com/id/1398348439/photo/black-lamborghini-huracan-performante.jpg?s=612x612&w=0&k=20&c=y1McG6jnpijKMZj9SXmw-TrT-PzS0OfV_eOoLKlYM5M=' 
+
+//     },
+    
+// ]
+
+// let showproduct= document.querySelector("#showproduct")
+// function showproduct1 () {
+//     for (let i = 0; i <arr.length; i++) {
+//         let getproduct = arr[i]
+
+//         let pid =document.createElement('p')
+//         let ptext = document.createTextNode(getproduct.p_id)
+//         pid.appendChild()
+
+//     }
+// }
