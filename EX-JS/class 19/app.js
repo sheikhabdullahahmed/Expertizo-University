9dc716e7cacf9ddafd3b44ef46fe5f878df1dd14
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-27 12:59:52 +0500

    continue

---
+++ EX-JS/class 19/app.js
@@ -0,0 +1,190 @@
+// let user = prompt('Enter Your Number');
+// function sum () {
+//     document.write(user * user.length+ "*" ) 
+
+// }
+
+// sum()
+
+
+// function factorial () {
+//     let num = 1
+//     for ()
+// } 
+
+
+// const a  = "Sheikh"
+// {
+//     const a = "Abdullah"
+//     console.log (a)
+// }
+// console.log (a)
+
+// function abc () {
+//     const a = "Ahmed"
+// }
+
+// console.log(a)
+
+// let a  = "Sheikh"
+// a = "Abdullah"
+// console.log(a)
+
+
+// const a =  "Abdullah"
+// a = 'Sheikh'
+// console.log(a)
+
+// let pi = 2.3;
+// pi = 0.9
+// console.log(pi)
+
+
+// abc () 
+// function abc() {
+//     alert("sheikh")
+// }
+
+
+// abc()
+// function abc() {
+//     alert("Sheikh")
+// }
+
+// console.log(a)
+// const a = "Abdullah" 
+
+
+// function sum (a = 100, b = 4 ) {
+//     return a * b 
+// }
+// if(sum > 400) {
+//     alert("hellow ")
+// }
+// console.log(sum())
+
+
+// palindtone matlab start or last kay word same ho etc.madam , maham,NaN;
+
+
+// let user = prompt ("Enter any word")
+// let check = ''
+
+// for (let i = user.length-1 ; i>=0; i--) {
+//     check+= user[i]  
+// }
+// if (check == user ) {
+//     alert("Word is palindtone")
+// }
+// else {
+//     alert("Word is not  palindtone")
+// }
+
+// let user = prompt ("Enter Any word")
+// let check = ''
+// for (let i = user.length-1; i>=0; i-- ) {
+//     check += user[i]
+// }
+// if (check == user) {
+//     alert('word is paridlontone')
+// }
+// else{
+//     alert("word is not paridlontone")
+// }
+
+// let user = prompt ("Enter Your Work")
+// let check = ''
+// for (let i = user.length-1; i>=0; i--) {
+//     check += user[i]
+// }
+// if (check == user) {
+//     alert("word is solid")
+// }
+// else {
+//     alert("word is unsolid")
+// }
+
+// let user = prompt ("enter your word")
+// let check  = ''
+// for (let i = user.length-1; i>=0; i--) {
+//     check+= user[i]
+// }
+// if (check == user ) {
+//     alert("word is solid")
+// }
+// else{
+//     alert("word is unsolid")
+// }
+
+// function factorial (n) {
+//     let num = 1
+//     for (let i = 2; i<=n; i++){
+//         num*=1
+//     }
+//     return num
+// }
+
+// console.log (factorial(6))
+
+
+
+// function factorial (n) {
+//     let num = 1
+//     for (let i = 2; i<=n; i++) {
+//         num*=1
+//     }
+//     return num
+// }
+
+
+
+// console.log (factorial ())
+
+
+
+// function factorial (n) {
+//     let num = 1
+//     for (let i = 2; i<=n; i++) {
+//         num*=4
+//     }
+//     return num
+// }
+
+// console.log (factorial ())
+
+
+// let user = prompt('enter any word')
+// let check  = ''
+
+// for (let i = user.length-1; i>=0; i--) {
+//     check += user[i]
+// }
+
+// if (check == user) {
+//     alert("WORD is solid")
+// }
+// else {
+//     alert('word is unsolid')
+// }
+
+
+// function factorial (n) {
+//     let num = 1
+//     for (let i = 2; i<=n; i++) {
+//         num*= 78
+//     }
+//     return num
+// }
+// console.log (factorial (3))
+
+
+
+// function factorial (n) {
+//     let num = 1
+//     for (let i = 2; i<=n; i++) {
+//         num*= 9 
+//     } 
+//     return num
+// }
+
+// console.log (factorial(2))
